6d9a01daf8ab695f1fffc99e19482003497fd0ed
David Liebman <radiodee1@gmail.com>
2024-04-08 16:41:33 -0400

    buffer enter

--- lua/pic-show/init.lua
+++ lua/pic-show/init.lua
@@ -3,11 +3,11 @@ local jpg = require("pic-show.jpg")
 
 local M = {}
 
-M.show_png = png.show 
-M.show_jpg = jpg.show 
-
 Allow_png = 0
 Allow_jpg = 0
+
+M.show_png = png.show 
+M.show_jpg = jpg.show 
 
 M.set_png = png.set 
 M.set_jpg = jpg.set 
