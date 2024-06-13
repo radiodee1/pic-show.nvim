d19f6dc13bcca8a2090dd4fcae94d9ea4d869e16
David Liebman <radiodee1@gmail.com>
2024-06-13 07:34:29 -0400

    any .gif

--- lua/pic-show/any.lua
+++ lua/pic-show/any.lua
@@ -2,7 +2,7 @@
 local M = {}
 
 function M.show()
-    local whitelist = {".png", ".jpg", ".jpeg", ".pdf", ".html", ".htm", ".xcf"}
+    local whitelist = {".png", ".jpg", ".jpeg", ".gif" ,".pdf", ".html", ".htm", ".xcf"}
     local filename = "\"" .. vim.fn.expand("%") .. "\""
     
     for _, i in ipairs(whitelist) do
